043f04f3957aad20a12ffe3a2705fbd85515c1a2
Kiley Davidson <kiley.davidson@elastic.co>
2020-07-08 14:29:19 -0700

    Added Generic Notices - UAMDM

---
+++ src/pages/genericnotices/start/index.tsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import {
+  EuiEmptyPrompt,
+  EuiButton,
+  EuiText,
+  EuiCode,
+  EuiSpacer
+} from '@elastic/eui';
+
+import triggerIcon from 'src/images/logo_menubartrigger.png';
+
+const render = () => (
+  <EuiEmptyPrompt
+    iconType={triggerIcon}
+    title={<h2>Well, that&apos;s not right</h2>}
+    body={
+      <Fragment>
+        <EuiText>
+          <p>
+            <em>Trigger</em> was launched with the <EuiCode>genericnotices/start</EuiCode> UX Path, but this module requires a direct path to a UX page.
+          </p>
+          <p>Generally, this is because of a scripting error on the part of your IT Administrator. She probably needs more coffee (or a nap).</p>
+          <p>Slack #IT for more help.</p>
+          </EuiText>
+          <EuiSpacer size="l" />
+          <EuiText size="m"><p>❤️☕️</p></EuiText>
+      </Fragment>
+    }
+    actions={
+      <a href="http://quit">
+        <EuiButton color="primary" fill>
+          Close Trigger
+        </EuiButton>
+      </a>
+    }
+  />
+);
+
+export default render;
---
+++ src/pages/genericnotices/uamdm/index.tsx
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react';
+import {
+  EuiEmptyPrompt,
+  EuiButton,
+  EuiText,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButtonEmpty
+} from '@elastic/eui';
+
+import triggerIcon from 'src/images/icon_ITComms.png';
+
+const render = () => (
+  <EuiEmptyPrompt
+    iconType={triggerIcon}
+    title={<h2>Action Required for Workstation Controls</h2>}
+    body={
+      <Fragment>
+        <EuiText>
+          <p>
+            Your Mac is only partially enrolled in <a href="https://wiki.elastic.co/display/AD/For+Macs%3A+JAMF+Pro">Workstation Controls.</a> This prevents IT Engineering from fully managing your Mac, which can create problems for you and Elastic's regulatory compliance.
+          </p>
+          <p>To fix this, you need to open System Preferences, click <em>Profiles</em>, select <em>Elastic JamfPro MDM</em>, and click <strong>Approve</strong>.</p>
+          <p>Check <a href="https://wiki.elastic.co/display/AD/For+Macs%3A+JAMF+Pro">the Elastic Wiki for more details</a>, or Slack #IT for help.</p>
+          </EuiText>
+      </Fragment>
+    }
+    actions={
+      <Fragment>
+        <EuiFlexGroup gutterSize="s" alignItems="center" direction="column">
+          <EuiFlexItem>
+            <EuiButton fill href="http://next">
+              Open System Preferences
+            </EuiButton>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiButtonEmpty color="danger" href="http://quit">
+              Remind Me Later
+            </EuiButtonEmpty>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </Fragment>
+    }
+  />
+);
+
+export default render;
